e58b49a0a67930cf9d916b17f6d2ca46f55537e2
Moises Cruz <moises.c.alcala@gmail.com>
2022-01-18 18:18:31 -0600

    Day 1: Band Name Generator

---
+++ Day_1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+string_1 = input("Whats name of the city you grew up in?\n")
+string_2 = input("What's your pet's name?\n")
+
+print(f"Your band name is: {string_1} {string_2}")
