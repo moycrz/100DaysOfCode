4a3dc8919a670db886d8a045f147e29f633b3892
Monica Onofre Abadía <moises.c.alcala@gmail.com>
2022-01-27 17:09:53 -0600

    Day 9: Updates

---
+++ Day_9.py
@@ -0,0 +1,19 @@
+from traceback import print_tb
+from Day_9_art import art
+
+def FunctionName(args):
+    there_are_bidders = True
+    bidders = []
+
+    while there_are_bidders:
+        print(art)
+        name = input("What is your name?: ")
+        offert = input("What is your bid?: $")
+
+        bidders
+
+
+
+if __name__ == "__main__":
+    
+    input("Are there any other bidders? Type 'yes or 'no'.")
