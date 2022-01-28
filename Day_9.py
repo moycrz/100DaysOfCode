1d2eeb5697f41c8be92b81c4f28d1cb9339b9496
Monica Onofre Abadía <moises.c.alcala@gmail.com>
2022-01-28 12:29:10 -0600

    Day 9: Blind Auction

--- Day_9.py
+++ Day_9.py
@@ -1,19 +1,40 @@
-from traceback import print_tb
-from Day_9_art import art
+import os
+from Day_9_art import logo
 
-def FunctionName(args):
+def add_bidders():
     there_are_bidders = True
-    bidders = []
+    bidders = {}
 
     while there_are_bidders:
-        print(art)
+        os.system("clear")
+        print(logo)
         name = input("What is your name?: ")
         offert = input("What is your bid?: $")
+        bidders [name] = offert
 
-        bidders
+        usr_answer = input("Are there any other bidders? Type 'yes or 'no': ").lower()
+        if usr_answer == "no":
+            find_highest_bidder(bidders)
+            there_are_bidders = False
 
+        else:
+            continue
+    
+
+def find_highest_bidder(bidders):
+    highest_bidder = 0
+
+    for key in bidders:
+        score = int(bidders[key])
+
+        if score > highest_bidder:
+            highest_bidder = score
+            higher_name = key
+        else:
+            continue
+
+    print(f"The winner is {higher_name} with a bid of ${highest_bidder}")
 
 
 if __name__ == "__main__":
-    
-    input("Are there any other bidders? Type 'yes or 'no'.")
+    add_bidders()
