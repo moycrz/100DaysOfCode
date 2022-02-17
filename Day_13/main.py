c6201200835f5b6f505acd20650fc1ed5f8ba08b
Moises Cruz <moises.c.alcala@gmail.com>
2022-02-16 19:25:25 -0600

    Day 13: Debugging

--- Day_13/main.py
+++ Day_13/main.py
@@ -34,11 +34,44 @@
 # print(total_words)
 
 #Use a Debugger
-def mutate(a_list):
-  b_list = []
-  for item in a_list:
-    new_item = item * 2
-  b_list.append(new_item)
-  print(b_list)
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#     b_list.append(new_item)
+#   print(b_list)
 
 # mutate([1,2,3,5,8,13])
+
+'''Excercise_1'''
+# number = int(input("Which number do you want to check?"))
+
+# if number % 2 == 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+  
+'''Excercise_2'''
+# year = int(input("Which year do you want to check?"))
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+
+'''Exercise_3''' 
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+        print("Fizz")
+  elif number % 5 == 0:
+        print("Buzz")
+  else:
+    print([number])
