18212ab55c8deb1f6803c5d8886396042c1c9305
Moises Cruz <moises.c.alcala@gmail.com>
2022-02-25 10:57:54 -0600

    Day 16: OOP Coffee Machine

--- Day_16/main.py
+++ Day_16/main.py
@@ -1,42 +1,62 @@
 from prettytable import PrettyTable
 
-import money_machine
 from coffee_maker import CoffeeMaker
 from menu import Menu, MenuItem
 from money_machine import MoneyMachine
 
 is_on = True
 coffee_machine = CoffeeMaker()
-# m_item = Menu.find_drink(choice)
-# print(m_item.cost())
+m_item = Menu()
+money_machine = MoneyMachine()
 
 menu_table = PrettyTable()
 menu_table.add_column("Coffee", ["Espresso", "Latte", "Cappuccino"])
-# menu_table.add_column("Price", [m_item("espresso"), m_item("latte"), m_item("cappuccino")])
+menu_table.add_column("Price", ["$1.5", "$2.5", "$3"])
 
-current_money = MoneyMachine
+espresso = MenuItem("espresso", 50, 0, 18, 1.5)
+latte = MenuItem("latte", 200, 150, 24, 2.5)
+cappuccino = MenuItem("cappuccino", 250, 50, 24, 3)
+
+
+def ingredients(coffee_choice):
+    return coffee_choice.ingredients
+
+
+def cost(coffee_type):
+    return coffee_type.cost
+
 
 while is_on:
-    # print(menu_table)
-    choice = input("What would you like? (espresso/latte/cappuccino/): ")
+    print(menu_table)
+    choice = input("What would you like? (espresso/latte/cappuccino/): ").lower()
 
     if choice == "off":
         is_on = False
 
     elif choice == "report":
         coffee_machine.report()
-        print(MoneyMachine.report())
+        print(money_machine.report())
 
     elif choice == "espresso":
-        if coffee_machine.is_resource_sufficient(choice):
-            coffee_machine.make_coffee(choice)
-        m_item = Menu.find_drink(choice)
-        print(m_item.cost())
+        if coffee_machine.is_resource_sufficient(espresso):
+            price = cost(espresso)
+            if money_machine.make_payment(price):
+                coffee_machine.make_coffee(espresso)
+            else:
+                continue
 
     elif choice == "latte":
-        if coffee_machine.is_resource_sufficient(choice):
-            coffee_machine.make_coffee(choice)
+        if coffee_machine.is_resource_sufficient(latte):
+            price = cost(latte)
+            if money_machine.make_payment(price):
+                coffee_machine.make_coffee(latte)
+            else:
+                continue
 
     elif choice == "cappuccino":
-        if coffee_machine.is_resource_sufficient(choice):
-            coffee_machine.make_coffee(choice)
+        if coffee_machine.is_resource_sufficient(cappuccino):
+            price = cost(cappuccino)
+            if money_machine.make_payment(price):
+                coffee_machine.make_coffee(cappuccino)
+            else:
+                continue
