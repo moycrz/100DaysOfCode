ca5356cb56cb3336bf9f1961b105f92f148cd69c
Moises Cruz <moises.c.alcala@gmail.com>
2022-02-24 20:57:24 -0600

    updates

--- Day_16/main.py
+++ Day_16/main.py
@@ -1,23 +1,42 @@
-# import another_module
-# print(another_module.another_variable)
-# from turtle import Turtle, Screen
-#
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("aquamarine")
-# timmy.forward(100)
-#
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
-
 from prettytable import PrettyTable
 
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
+import money_machine
+from coffee_maker import CoffeeMaker
+from menu import Menu, MenuItem
+from money_machine import MoneyMachine
 
+is_on = True
+coffee_machine = CoffeeMaker()
+# m_item = Menu.find_drink(choice)
+# print(m_item.cost())
 
-print(table)
+menu_table = PrettyTable()
+menu_table.add_column("Coffee", ["Espresso", "Latte", "Cappuccino"])
+# menu_table.add_column("Price", [m_item("espresso"), m_item("latte"), m_item("cappuccino")])
 
+current_money = MoneyMachine
+
+while is_on:
+    # print(menu_table)
+    choice = input("What would you like? (espresso/latte/cappuccino/): ")
+
+    if choice == "off":
+        is_on = False
+
+    elif choice == "report":
+        coffee_machine.report()
+        print(MoneyMachine.report())
+
+    elif choice == "espresso":
+        if coffee_machine.is_resource_sufficient(choice):
+            coffee_machine.make_coffee(choice)
+        m_item = Menu.find_drink(choice)
+        print(m_item.cost())
+
+    elif choice == "latte":
+        if coffee_machine.is_resource_sufficient(choice):
+            coffee_machine.make_coffee(choice)
+
+    elif choice == "cappuccino":
+        if coffee_machine.is_resource_sufficient(choice):
+            coffee_machine.make_coffee(choice)
