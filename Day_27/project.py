b3578f65ba45e6627d8f7ae7bd36819ff1042da2
moycrz <moises.c.alcala@gmail.com>
2022-04-13 12:07:33 -0500

    update

--- Day_27/project.py
+++ Day_27/project.py
@@ -1,39 +1,39 @@
 from tkinter import *
 
 
-def button_clicked():
-    miles_to_km = round(int(input_miles.get()) / 0.62137, 2)
-    num_km_label.config(text=miles_to_km)
+def miles_to_km():
+    miles = float(input_miles.get())
+    km = round(miles / 0.62137, 2)
+    num_km_label.config(text=km)
 
 
 window = Tk()
 window.title("Mile to Km Converter")
-window.minsize(width=300, height=150)
-window.config(padx=50, pady=100)
+window.config(padx=20, pady=20)
 
 # Entry
 input_miles = Entry(width=10, justify="center")
 input_miles.grid(column=1, row=0)
 
-# Label
+# Label 'Miles'
 miles_label = Label(text="Miles")
 miles_label.grid(column=2, row=0)
 
-# Label
+# Label "is equal to"
 equal_label = Label(text="is equal to")
 equal_label.grid(column=0, row=1)
 
-# Label variable
+# Label convert result
 num_km_label = Label(text="0")
 num_km_label.grid(column=1, row=1)
 num_km_label.config(padx=10, pady=20)
 
-# Label
-km_my_label = Label(text="Km")
-km_my_label.grid(column=2, row=1)
+# Label 'Km'
+km_result_label = Label(text="Km")
+km_result_label.grid(column=2, row=1)
 
 # Button
-button = Button(text="Click Me", command=button_clicked)
+button = Button(text="Calculate", command=miles_to_km)
 button.grid(column=1, row=2)
 
 
