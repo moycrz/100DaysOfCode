ace9f37bd3e341582d6146e1a63b3b5bd43cb79c
moycrz <moises.c.alcala@gmail.com>
2022-04-20 12:20:40 -0500

    Day 30: Error, Exceptions and JSON Data: Improving the Password (Day 29).

--- Day_26/main.py
+++ Day_26/main.py
@@ -1,17 +1,18 @@
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
 import pandas
 
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
-# TODO 1. Create a dictionary in this format:
-#  {"A": "Alfa", "B": "Bravo"}
 
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        phonetic_word = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phonetic_word)
+
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
-usr_word = list(input("type a word: ").upper())
-print(usr_word)
-phonetic_word = [phonetic_dict[f"{word}"] for word in usr_word if word in phonetic_dict]
-print(phonetic_word)
+generate_phonetic()
--- Day_29/main.py
+++ Day_29/main.py
@@ -2,11 +2,32 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=f"{website.title()}", message=f"Email: {email}\nPassword: {password}")
+            pyperclip.copy(password)
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -32,25 +53,37 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = pass_entry.get()
+    new_data = {website: {
+        "email": email,
+        "password": password
+    }}
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any field empty!")
-
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\nEmail: {email}"
-                                                              f"\nPassword: {password} \n Is it ok to save?")
-        if is_ok:
-            with open("data.txt", mode="a") as data:
-                data.write(f"\n{website}, {email}, {password}")
-                website_entry.delete(0, END)
-                pass_entry.delete(0, END)
-                website_entry.focus()
+        try:
+            with open("data.json", mode="r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", mode="w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+            with open("data.json", mode="w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            pass_entry.delete(0, END)
+            website_entry.focus()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=50, pady=50)
+window.config(padx=30, pady=30)
 
 # LOGO
 canvas = Canvas(width=200, height=200)
@@ -60,30 +93,34 @@ canvas.grid(column=1, row=0)
 
 # Website LABEL and ENTRY
 website_label = Label(text="Website:")
-website_entry = Entry(width=45)
+website_entry = Entry(width=26)
 website_entry.focus()
 website_label.grid(column=0, row=1)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1)
 
 # Email/Username LABEL and ENTRY
 email_label = Label(text="Email/Username:")
-email_entry = Entry(width=45)
+email_entry = Entry(width=44)
 email_entry.insert(0, "moises.c.alcala@gmail.com")
 email_label.grid(column=0, row=2)
 email_entry.grid(column=1, row=2, columnspan=2)
 
 # Password LABEL and ENTRY
 pass_label = Label(text="Password:")
-pass_entry = Entry(width=27)
+pass_entry = Entry(width=26)
 pass_label.grid(column=0, row=3)
 pass_entry.grid(column=1, row=3)
 
 # Password BUTTON
-pass_button = Button(text="Generate Password", command=generate_password)
+pass_button = Button(text="Generate Password", command=generate_password, width=14)
 pass_button.grid(column=2, row=3)
 
+# Website BUTTON
+website_button = Button(text="Search", command=find_password, width=14)
+website_button.grid(column=2, row=1)
+
 # ADD BUTTON
-add_button = Button(text="Add", width=40, command=save)
-add_button.grid(column=1, row=4, columnspan=2)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(column=1, row=4, columnspan=2, )
 
 window.mainloop()
